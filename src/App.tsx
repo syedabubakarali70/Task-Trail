2eebd894e2d93fffca8a56f9efd28bb30703f3a0
Syed Abubakar Ali <syedabubakarali@gmail.com>
2024-09-16 12:23:36 +0500

    Created buttons and paper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Paper } from "./components/ui/paper";
+import ThemeToggle from "./components/ThemeToggle";
 
 const App = () => {
-  return <Button>Hello World</Button>;
+  return (
+    <div className="bg-surface h-[100vh]">
+      <ThemeToggle />
+      <Button variant="elevated">Elevated</Button>
+      <Button variant="filled">Filled</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button variant="ghost">Text / ghost</Button>
+      <Button variant="filledTonal">Filled Tonal</Button>
+
+      <div className="flex justify-between h-96] ">
+        <Paper elevation={1} className="p-4 w-40 h-56 "></Paper>
+        <Paper elevation={2} className="p-4 w-40 h-56 "></Paper>
+        <Paper elevation={3} className="p-4 w-40 h-56 "></Paper>
+        <Paper elevation={4} className="p-4 w-40 h-56 "></Paper>
+        <Paper elevation={5} className="p-4 w-40 h-56 "></Paper>
+        <Paper elevation={6} className="p-4 w-40 h-56 "></Paper>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,22 @@
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import { useTheme } from "@/components/Providers/themeProvider";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <Button
+      onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
+      size="icon"
+    >
+      {theme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] " />
+      )}
+    </Button>
+  );
+};
+
+export default ThemeToggle;
